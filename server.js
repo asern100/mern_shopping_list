5826e0543b7a500a5a162d26b205d5549a7714bb
Ahmed <saiefeddina@gmail.com>
2021-07-02 12:23:43 +0100

    add items model & routes

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const express = require('express')
 const mongoose = require('mongoose')
 const bodyParser = require('body-parser')
 
+const items = require('./routes/api/items')
+
 const app = express();
 
 // BodyParser Middlware
@@ -17,6 +19,9 @@ mongoose.connect(db, { useNewUrlParser: true, useUnifiedTopology: true })
     .then(()=> console.log('MongoDB Connected'))
     .catch(err=> console.log(err));
 
+
+app.use('/api/items', items)
+    
 const port =process.env.PORT || 5000;
 
 app.listen(port, ()=> console.log(`Server started on port ${port}`));
